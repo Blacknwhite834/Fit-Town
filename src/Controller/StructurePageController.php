87457c1947139d4776fa1e6af2b1d5ffde1b8566
Evan Le coz <evanlecoz6@gmail.com>
2022-10-29 20:24:01 +0200

    Search structure page

--- src/Controller/StructurePageController.php
+++ src/Controller/StructurePageController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Partenaire;
 use App\Repository\StructureRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,20 +18,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class StructurePageController extends AbstractController
 {
     #[Route('/structure-page', name: 'app_structure_page', methods: ['GET'])]
-    public function index(StructureRepository $structureRepository): Response
+    public function index(StructureRepository $structureRepository,  Request $request, EntityManagerInterface $entityManager): Response
     {
+        $partenaireId = $entityManager->getRepository(Partenaire::class)->findOneBy([
+            'id' => $this->getUser()->getId(),
+        ]);
 
         $getName = $this->getUser()->getName();
         $getEmail = $this->getUser()->getEmail();
+
+        $search2 = $structureRepository->findOneBySomeField2(
+            $partenaireId,
+            $request->query->get('q')
+        );
 
         return $this->render('structure_page/index.html.twig', [
             'controller_name' => 'StructurePageController',
             'name'=>$getName,
             'email'=>$getEmail,
-            'structures'=>$structureRepository->findBy(
-                ['is_active' => true, 'partenaire' => $this->getUser()->getId()],
-                [],
-            ),
+            'structures'=>$search2,
+
         ]);
     }
 }
